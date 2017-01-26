f6d89e766715e2b370fb9111d64a998cd1080b92
dingliheng <liheng.ding@utexas.edu>
2017-01-25 19:19:09 -0600

    change the zigzag iterator

--- ZigzagIterator.java
+++ ZigzagIterator.java
@@ -1,35 +1,19 @@
-c class ZigzagIterator {
-    
-    int counter;
-    List<Integer> small;
-    List<Integer> big;
+public class ZigzagIterator {
+    LinkedList<Iterator> list;
     public ZigzagIterator(List<Integer> v1, List<Integer> v2) {
-        counter = 0;
-        small = v1;
-        big = v2;
+        list = new LinkedList<Iterator>();
+        if(!v1.isEmpty()) list.add(v1.iterator());
+        if(!v2.isEmpty()) list.add(v2.iterator());
     }
 
     public int next() {
-        if (counter >= big.size() * 2) {
-            return small.get(counter++ - big.size());
-        }
-        if (counter >= small.size() * 2) {
-            return big.get(counter++ - small.size());
-        }
-        if (counter % 2 == 0) {
-            return small.get(counter++ / 2);
-        }else {
-            return big.get(counter++ / 2);
-        }
+        Iterator poll = list.remove();
+        int result = (Integer)poll.next();
+        if(poll.hasNext()) list.add(poll);
+        return result;
     }
 
     public boolean hasNext() {
-        return counter < small.size() + big.size();
+        return !list.isEmpty();
     }
 }
-
-/**
- * Your ZigzagIterator object will be instantiated and called as such:
- * ZigzagIterator i = new ZigzagIterator(v1, v2);
- * while (i.hasNext()) v[f()] = i.next();
- */
